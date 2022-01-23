4850e5933c42b7370b1ac57144ead0c54d22b835
reza ramezani <rezaaa555555r@gmail.com>
2022-01-23 21:29:18 +0330

    add: Multilingual menus and hide desktop navbar in small sizes

--- components/layout/app-header.vue
+++ components/layout/app-header.vue
@@ -1,22 +1,58 @@
 <template>
-  <header class="flex justify-between h-[100px] items-center">
+  <header
+    class="flex rtl:flex-row-reverse justify-between h-[70px] md:h-[100px] items-center"
+  >
     <div>
-      <span class="text-[#BBE1FA] text-[30px]">Logooo</span>
+      <span class="text-brand-7 text-[30px]">Logooo</span>
     </div>
-    <div>
-      <nav class="flex gap-x-5 xl:gap-x-8 text-white text-[18px] xl:text-[20px]">
-        <NuxtLink v-for="link in navBarLinks" :key="link.href" class="nav-item select-none" draggable="false" :to="link.href">
+    <div class="hidden md:block">
+      <nav
+        class="flex gap-x-5 xl:gap-x-8 text-white text-[18px] xl:text-[20px]"
+      >
+        <NuxtLink
+          v-for="link in navBarLinks"
+          :key="link.href"
+          class="nav-item select-none"
+          draggable="false"
+          :to="localePath(link.href, $i18n.locale)"
+        >
           {{ link.lable }}
         </NuxtLink>
       </nav>
     </div>
-    <div class="flex items-center gap-x-3">
+    <div class=" items-center gap-x-3 rtl:flex-row-reverse hidden md:flex">
       <button>
-        <svg width="26" height="30" viewBox="0 0 39 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M11.3611 28.0833C6.7434 28.0833 3 31.8267 3 36.4444V40.625M36.4444 40.625V36.4444C36.4444 31.8267 32.701 28.0833 28.0833 28.0833H19.7222M19.7222 3C15.1045 3 11.3611 6.7434 11.3611 11.3611C11.3611 15.9788 15.1045 19.7222 19.7222 19.7222C24.3399 19.7222 28.0833 15.9788 28.0833 11.3611C28.0833 9.83819 27.6762 8.41036 26.9648 7.18056" stroke="white" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" />
+        <svg
+          width="26"
+          height="30"
+          viewBox="0 0 39 44"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M11.3611 28.0833C6.7434 28.0833 3 31.8267 3 36.4444V40.625M36.4444 40.625V36.4444C36.4444 31.8267 32.701 28.0833 28.0833 28.0833H19.7222M19.7222 3C15.1045 3 11.3611 6.7434 11.3611 11.3611C11.3611 15.9788 15.1045 19.7222 19.7222 19.7222C24.3399 19.7222 28.0833 15.9788 28.0833 11.3611C28.0833 9.83819 27.6762 8.41036 26.9648 7.18056"
+            stroke="white"
+            stroke-width="5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
         </svg>
       </button>
-      <select class="outline-none uppercase cursor-pointer flex pt-2 px-2 text-[18px] xl:text-[20px] bg-transparent text-white" @change="$router.push(switchLocalePath($event.target.value))">
+      <select
+        class="
+          outline-none
+          uppercase
+          cursor-pointer
+          flex
+          pt-2
+          px-2
+          text-[18px]
+          xl:text-[20px]
+          bg-transparent
+          text-white
+        "
+        @change="$router.push(switchLocalePath($event.target.value))"
+      >
         <option
           v-for="locale in $i18n.locales"
           :key="locale.code"
@@ -35,40 +71,67 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'LayoutAppHeader',
-  data: () => ({
-    navBarLinks: [
-      {
-        lable: 'Main page',
-        href: '/'
-      },
-      {
-        lable: 'Serveces',
-        href: '#serveces'
-      },
-      {
-        lable: 'About us',
-        href: '#about-us'
-      },
-      {
-        lable: 'Calculator',
-        href: '/calculator'
-      },
-      {
-        lable: 'Mag',
-        href: '/mag'
-      },
+  computed: {
+    navBarLinks () {
+      return [
+        {
+          lable: this.$i18n.t('links.mainPage'),
+          href: '/'
+        },
+        {
+          lable: this.$i18n.t('links.serveces'),
+          href: '/#serveces'
+        },
+        {
+          lable: this.$i18n.t('links.aboutUs'),
+          href: '/#about-us'
+        },
+        {
+          lable: this.$i18n.t('links.calculator'),
+          href: '/calculator'
+        },
+        {
+          lable: this.$i18n.t('links.mag'),
+          href: '/mag'
+        },
 
-      {
-        lable: 'Contact us',
-        href: '/contact-us'
-      }
-    ]
-  })
+        {
+          lable: this.$i18n.t('links.contactUs'),
+          href: '/contact-us'
+        }
+      ]
+    }
+  }
 })
 </script>
 
 <style scoped>
-    .nav-item.nuxt-link-exact-active {
-        @apply border-b-[3px] border-brand-4;
-    }
+.nav-item.nuxt-link-exact-active {
+  @apply border-b-[3px] border-brand-4;
+}
 </style>
+
+<i18n>
+{
+  "en": {
+    "links": {
+    "mainPage": "Main page",
+    "serveces": "Serveces",
+    "aboutUs": "About us",
+    "calculator": "Calculator",
+    "mag": "Mag",
+    "contactUs": "Contact us"
+    }
+  },
+  "fa": {
+    "links": {
+    "mainPage": "صفحه اصلی",
+    "serveces": "خدمات",
+    "aboutUs": "درباره ما",
+    "calculator": "حساب گر",
+    "mag": "مجله",
+    "contactUs": "تماس با ما"
+    }
+  }
+}
+</i18n>
