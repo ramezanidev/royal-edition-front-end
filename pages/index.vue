6e0ce2102cca8ace0cfd4c43393edb2aab76769c
reza ramezani <rezaaa555555r@gmail.com>
2022-01-23 21:30:48 +0330

    add: Multilingual texts and responsive small sizes

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,90 @@
 <template>
-  <div>
-    <NuxtLink
-      v-for="locale in $i18n.locales"
-      :key="locale.code"
-      :to="switchLocalePath(locale.code)"
-    >
-      {{ locale.name }}
-    </NuxtLink>
-    <h1 class="text-3xl font-bold underline text-red-800">
-      {{ $t("welcome") }}
-    </h1>
+  <div class="w-full h-full flex flex-col-reverse justify-center md:grid grid-cols-3 gap-x-6 items-center">
+    <div class="col-start-1 col-end-3 text-center md:rtl:text-right  md:ltr:text-left mt-8 md:mt-0">
+      <h2 class="text-3xl mb-6 font-extrabold text-brand-3">
+        {{ $t("title") }}
+      </h2>
+      <p class="text-brand-5 text-lg mb-8 max-w-xl">
+        {{ $t("description") }}
+      </p>
+      <div class="flex gap-x-6 justify-center md:justify-start">
+        <button
+          class="
+            bg-brand-7
+            flex
+            items-center
+            justify-evenly
+            select-none
+            rounded-xl
+            w-36
+            h-12
+            p-2
+            text-brand-3 font-bold
+          "
+        >
+          <span> {{ $t("links.calculator") }}</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            class="w-[24px] h-[24px]"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h8zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4z"
+            />
+            <path
+              d="M4 2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-2zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-4z"
+            />
+          </svg>
+        </button>
+        <button
+          class="
+            bg-brand-7
+            flex
+            items-center
+            justify-evenly
+            select-none
+            rounded-xl
+            w-36
+            h-12
+            p-2
+            text-brand-3 font-bold
+          "
+        >
+          <span> {{ $t("links.services") }}</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            class="w-[24px] h-[24px]"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"
+            />
+            <path
+              d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+    <div class="w-full md:w-auto flex md:block justify-center">
+      <div
+        class="
+          aspect-square
+          bg-brand-5
+          md:rounded-[60px] rounded-[45px]
+          overflow-hidden
+          w-full
+          max-w-[400px]
+          md:p-10 p-6 md:ltr:ml-auto md:rtl:mr-auto
+        "
+      >
+        <div class="w-full h-full bg-brand-6 md:rounded-[60px] rounded-[20px] flex">
+          <span class="m-auto text-brand-5 text-2xl">Main Image</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,3 +95,24 @@ export default Vue.extend({
   name: 'IndexPage'
 })
 </script>
+
+<i18n>
+{
+  "en": {
+    "title": "Lorem ipsum dolor sit amet",
+    "description": "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio molestias dicta eum optio aut, veritatis sequi repudiandae, voluptatibus tempore quos excepturi quasi corporis repellat amet, rem quidem laborum! Ullam",
+    "links": {
+      "services": "Services",
+      "calculator": "Calculator"
+    }
+  },
+  "fa": {
+    "title": "لورم ایپسوم متن ساختگی",
+    "description": "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای",
+    "links": {
+      "services": "خدمات",
+      "calculator": "حساب گر"
+    }
+  }
+}
+</i18n>
