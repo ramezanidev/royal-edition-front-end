03523eda745fba085fe11b918e7d7f8277ac05e4
reza ramezani <rezaaa555555r@gmail.com>
2022-01-27 20:33:57 +0330

    add: added animation on change section (#main-page)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="bg-brand-3 h-screen px-[20px] pb-[20px] md:px-[40px] md:pb-[40px] xl:px-[50px] xl:pb-[50px] 2xl:px-[50px] 2xl:pb-[50px] flex flex-col">
     <LayoutAppHeader />
-    <div class="rounded-[25px] flex-auto flex items-center relative p-[40px] xl:px-[60px] 2xl:px-[80px] bg-gradient-radial from-brand-1 to-brand-2" style="height:calc(100% - 100px)">
+    <div class="rounded-[25px] flex-auto flex items-center relative bg-gradient-radial from-brand-1 to-brand-2" style="height:calc(100% - 100px)">
       <Nuxt />
+      <button
+        class="bg-gradient-to-b shadow-lg backdrop-blur-[5px] backdrop-filter from-[#003050bd] to-[#0f4c75c0] absolute rounded-full items-center justify-center md:bottom-4 -bottom-3 left-1/2 md:left-6 transform -translate-x-1/2 md:translate-x-0 md:w-12 md:h-12 w-10 h-10 flex"
+        @click="step++"
+      >
+        <svg class="w-4" viewBox="0 0 26 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M13.7924 16.9706C13.3921 17.4906 12.6079 17.4906 12.2076 16.9706L0.382967 1.60999C-0.12323 0.952425 0.345533 0 1.17537 0L24.8246 0C25.6545 0 26.1232 0.952425 25.617 1.60999L13.7924 16.9706Z" fill="white" />
+        </svg>
+      </button>
     </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,11 @@
 <template>
-  <div class="w-full h-full">
-    <MainPageIntroduction v-if="step === 1" @next="step++" />
-    <MainPageServices v-else-if="step === 2" @previous="step--" />
-    <button
-      class="bg-gradient-to-b shadow-lg backdrop-blur-[5px] backdrop-filter from-[#003050bd] to-[#0f4c75c0] absolute rounded-full items-center justify-center md:bottom-4 -bottom-3 left-1/2 md:left-6 transform -translate-x-1/2 md:translate-x-0 md:w-12 md:h-12 w-10 h-10 flex"
-      @click="step++"
-    >
-      <svg class="w-4" viewBox="0 0 26 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M13.7924 16.9706C13.3921 17.4906 12.6079 17.4906 12.2076 16.9706L0.382967 1.60999C-0.12323 0.952425 0.345533 0 1.17537 0L24.8246 0C25.6545 0 26.1232 0.952425 25.617 1.60999L13.7924 16.9706Z" fill="white" />
-      </svg>
-    </button>
+  <div class="w-full h-full overflow-hidden absolute inset-0 rounded-[25px] p-[40px] xl:px-[60px] 2xl:px-[80px]">
+    <transition :name="animation">
+      <MainPageIntroduction v-show="step === 1" @next="nextSection" />
+    </transition>
+    <transition :name="animation">
+      <MainPageServices v-show="step === 2" @previous="previousSection" />
+    </transition>
   </div>
 </template>
 
@@ -19,7 +15,61 @@ import Vue from 'vue'
 export default Vue.extend({
   name: 'IndexPage',
   data: () => ({
-    step: 2 /* active section */
-  })
+    step: 1 /* active section */,
+    animation: 'fade',
+    isAnimation: false
+  }),
+  watch: {
+    step (a, b) {
+      this.animation = a > b ? 'slide' : 'fade'
+      // stop change sections
+      this.isAnimation = true
+      setTimeout(() => {
+        this.isAnimation = false
+      }, 1000)
+    }
+  },
+  methods: {
+    nextSection () {
+      if (!this.isAnimation) {
+        this.step++
+      }
+    },
+    previousSection () {
+      if (!this.isAnimation) {
+        this.step--
+      }
+    }
+  }
 })
 </script>
+
+<style>
+
+.fade-enter-active, .fade-leave-active {
+  transition: 1000ms;
+}
+.fade-enter{
+  transform: translateY(-100%);
+}
+.fade-enter-to, .fade-leave {
+  transform: translateY(0%);
+}
+.fade-leave-to {
+  transform: translateY(100%);
+}
+
+.slide-enter-active, .slide-leave-active {
+  transition: 1000ms;
+}
+.slide-enter{
+  transform: translateY(100%);
+}
+.slide-enter-to, .slide-leave {
+  transform: translateY(0%);
+}
+.slide-leave-to {
+  transform: translateY(-100%);
+}
+
+</style>
